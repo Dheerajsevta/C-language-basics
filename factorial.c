5ef306e6a69312e66c89cba766a8c0859d42ae97
Dheeraj Sevta <sevtadheeraj@gmail.com>
2024-11-06 19:14:16 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,17 @@
+//a program to find factorial of a number given by user 
+#include <stdio.h>
+void main(){
+    int num , fact = 1;
+    printf("enter number to find factorial\n");
+    //taking input 
+    scanf("%d",&num);
+    //loop to calculate factorial
+    for (int i = 1; i <= num; i++)
+    {
+        fact *= i ;
+    
+        
+    }
+    printf("final factorial is = %d\n" , fact);
+    
+}
